5847b9a3ec32eed43810f5eda50ff67e4292e683
Emmanuel Adedeji <emmans4destiny@gmail.com>
2020-10-03 01:28:13 +0300

    templates

---
+++ realestate/src/components/Top/Top.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+
+const Top=()=> {
+  return (
+    <div>
+      <div>
+        <h1>Lagos Property Finder</h1>
+        <p>Bringing Lagos property right to your fingertips</p>
+      </div>
+    </div>
+  )
+}
+
+export default Top
---
+++ realestate/src/pages/Home/index.js
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+import Top from '../../components/Top'
+
+const Home = () =>{
+  return <Top />
+}
+
+export default Home
