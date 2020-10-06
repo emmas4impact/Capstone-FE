4bc3161c08320e2a1eb067bdb868fa428bc7d22c
Emmanuel Adedeji <emmans4destiny@gmail.com>
2020-10-06 22:31:33 +0300

    Delete PropertyListingsConsumer.js

--- realestate/src/context/PropertyListingsConsumer.js
+++
@@ -1,8 +0,0 @@
-import React, {createContext}  from 'react';
-
-const DefaultState ={
-    propertyListings: []
-}
-
-const PropertyListingsContext = createContext(DefaultState)
-export const PropertyListingsConsumer = PropertyListingsContext.Consumer
