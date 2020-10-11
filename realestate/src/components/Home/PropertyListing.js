987a1684a025709d4b0fb5c6551bbceba179c3f8
Emmanuel Adedeji <emmans4destiny@gmail.com>
2020-10-11 21:38:56 +0300

    Update PropertyListing.js

--- realestate/src/components/Home/PropertyListing.js
+++ realestate/src/components/Home/PropertyListing.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
-import { Jumbotron, Card, Button , Container, Row, Col}from 'react-bootstrap'
+import {  Card,  Container, Row, Col} from 'react-bootstrap'
 import { connect } from "react-redux";
 import { Link, withRouter} from 'react-router-dom';
+import Maps from '../map/Map'
 //import StarRatingComponent from 'react-star-rating-component';
 
 
@@ -20,111 +21,30 @@ const getListingWithThunk = (listings) => {
     return async(dispatch, getState) => {
         const data= await fetch(`${BASE_URL}/listings`)
         listings  = await data.json()
-       
         console.log("A thunk was used to dispatch this action", getState());
         dispatch({
             type: "GET_PROPERTY",
             payload: listings.data,
         });
-        //console.log("hello", listings)
-    
+       
     };
   };
 
 
 class PropertyListing extends Component {
     
-    
-    
     componentDidMount = async (id) =>{
         this.props.getListingThunk(id)
         
-        // const response = await fetch("http://localhost:3456/students")
-        // const data = await response.json()
-        
-        // console.log(data)
-        // this.setState({
-        //     students: data.Students,
-        //     Total: data.Total
-        // })
-
-        // const resp = await fetch("http://localhost:3456/projects")
-        // const project = await resp.json()
-        // this.setState({
-            
-        //     totalProject: project.total,
-        //     Projects: project.projects
-        // })
-        
-       
     }
-    
-    //  handleClose = () => {
-    //      this.setState({
-    //          show: false
-    //      })
-    //  };
-    //  handleShow = () => {
-    //     this.setState({
-    //         show: true
-    //     })
-    //  }
-    //  saveStudent = async(e) =>{
-    //     e.preventDefault()
-    //     const resp = await fetch("http://localhost:3456/students",{
-    //         method: "POST",
-    //         body: JSON.stringify(this.state.student),
-    //         headers: {
-    //             "Content-Type": "application/json",
-    //         }
-    //     })
-        
-    //     if(resp.ok){
-    //         alert("New Student Added!");
-            
-    //         this.setState({
-    //             isLoading: false,
-    //             errMess: "",
-    //             student:{
-    //                 firstname: "",
-    //                 surname: "",
-    //                 email: "",
-    //                 dateofbirth: ""
-    //             }
-    //         })
-            
-    //     }else{
-    //         let json =await resp.json();
-    //         this.setState({
-    //             isLoading: false,
-    //             errMess: json.message
-    //         })
-    //     }
-         
-    //  }
-    //  handleChange=(input) =>{
-    //     let student =this.state.student;
-    //     let currentId = input.currentTarget.id;
-        
-    //     if(currentId){
-    //         student[currentId] =input.currentTarget.value;
-    //     }
-        
-    //     this.setState({student: student})
-    //  }
-    
     render(){
       
         return(
             <>
             
-            
-            <Jumbotron>
-               <h1>Properties</h1>  
-               <div style={{ display: "flex"}} onClick={this.handleShow}><Button>Add Property</Button></div>   
-            </Jumbotron>
+           
             <Container>
-            <Row>
+            <Row className="mt-5">
              
             {this.props.data.properties.map((property, i)=>(
                
@@ -134,8 +54,13 @@ class PropertyListing extends Component {
                         <Card.Body>
                             <Card.Title>{property.title}</Card.Title>
                             <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
+                             {property.description}
+                            </Card.Text>
+                            <Card.Text>
+                             {property.location.formattedAddress}
+                            </Card.Text>
+                            <Card.Text>
+                             {property.region}
                             </Card.Text>
                             <Link className="btn btn-primary" to={`/properties/${property._id}`}>
                             Go To property
@@ -150,7 +75,8 @@ class PropertyListing extends Component {
             ))}
             
             </Row>
-       
+            <div> <h1>Properties Location</h1></div>
+             <Maps/>               
             </Container>  
 
           
